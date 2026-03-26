14c9bb3a79df330759e32b62f730f527efc7dd6b
Mikołaj Pęcak <mikolajpecak@gmail.com>
2026-03-26 18:27:18 +0100

    Change title of a push notification

--- services/api/notifyReceiverOfNewMessage.service.ts
+++ services/api/notifyReceiverOfNewMessage.service.ts
@@ -58,7 +58,7 @@ export async function notifyReceiverOfNewMessage(params: {
         : params.messagePreview;
 
     const payload = JSON.stringify({
-      title: `New message from ${params.senderEmail}`,
+      title: params.senderEmail,
       body: preview,
       url: '/dashboard',
       tag: `message-${params.messageId}`,
